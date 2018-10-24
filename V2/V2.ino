92d9578e4b894f77a2dfb74b55ebff57cebe97dc
Kate Granstrom <kdgranstrom@uwaterloo.ca>
2018-10-23 20:42:23 -0400

    added notes

--- V2/V2.ino
+++ V2/V2.ino
@@ -1,6 +1,31 @@
+int C = 262;
+int Cs = 277;
+int D = 294;
+int Ds = 311;
+int E = 330;
+int F = 349;
+int Fs = 370;
+int G = 392;
+int Gs = 415;
+int A = 440;
+int As = 466;
+int B = 494;
+int C = 523;
+
+int keyC = 10;
+int keyD = 9;
+int keyE = 8;
+int keyF = 7;
+int keyG = 6;
+int keyA = 5;
+int keyB = 4;
+int highkeyC = 3;
+
+int buzzer = 11;
+
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
